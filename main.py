19e10b46839846d4fe2ba2ffc0b5b7e526a8e93b
moiova <pingucom1@gmail.com>
2024-05-11 10:43:13 +0200

    code improvements and saving cards learned progress

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from tkinter import *
 from tkinter import messagebox
 import pandas
 import random
+import os
 
 data_as_dict = {}
+current_card = {}
 
 BACKGROUND_COLOR = "#B1DDC6"
 TIMER = 5000
-current_fr_word = None
+
 
 # ------------------ Buttons Listeners -----------------#
 
@@ -19,37 +21,37 @@ def failed():
 def remembered():
     # as long as the dict is not empty
     if data_as_dict:
-        del data_as_dict[current_fr_word]
+        data_as_dict.remove(current_card)
+        # save progress on file
+        frame = pandas.DataFrame(data_as_dict)
+        frame.to_csv("data/words_to_learn.csv")
+
         flip_to_front_side()
 
     else:  # no more entries available
         win.after_cancel(flip_timer)
-        messagebox.showinfo(title="Congratulations!", message="Good job! All flash cards remembered!")
 
 
 # ------------------- flip the card ------------------#
 def flip_to_back_side():
     canvas.itemconfig(canvas_image, image=back_image)
-    canvas.itemconfigure(language_id, text="English")
+    canvas.itemconfig(language_id, text="English", fill="white")
     try:
-        canvas.itemconfigure(word_id, text=f"{data_as_dict[current_fr_word]}")
+        canvas.itemconfig(word_id, text=current_card["English"], fill="white")
     except KeyError:
         messagebox.showinfo(title="Congratulations!", message="Good job! All flash cards remembered!")
 
 
 def flip_to_front_side():
-    global flip_timer
-    global current_fr_word
+    global flip_timer, current_card
 
     win.after_cancel(flip_timer)
-    current_fr_word = next_word()
-    if current_fr_word:
+    current_card = next_card()
+    if current_card:
         canvas.itemconfig(canvas_image, image=front_image)
-        canvas.itemconfigure(language_id, text="French")
-        canvas.itemconfigure(word_id, text=f"{current_fr_word}")
+        canvas.itemconfig(language_id, text="French", fill="black")
+        canvas.itemconfig(word_id, text=current_card["French"], fill="black")
         flip_timer = win.after(TIMER, flip_to_back_side)
-    else:
-        messagebox.showinfo(title="Congratulations!", message="Good job! All flash cards remembered!")
 
 
 # ------------------- read data from csv -------------#
@@ -57,37 +59,40 @@ def read_data_from_csv_as_dict():
     global data_as_dict
 
     try:
-        data = pandas.read_csv("data/french_words.csv")
+        data = pandas.read_csv("data/words_to_learn.csv")
+
     except FileNotFoundError:
-        messagebox.showerror(title="Error", message="File does not exist")
+        try:
+            complete_data = pandas.read_csv("data/french_words.csv")
+            data_as_dict = complete_data.to_dict(orient="records")
+        except FileNotFoundError:
+            messagebox.showerror(title="Error", message="File does not exist")
     except pandas.errors.EmptyDataError:
         messagebox.showerror("Error", message="No data available in the file")
 
     else:
         # consider all French words as keys and the corresponding english words as values
-        for (index, row) in data.iterrows():
-            data_as_dict[row.French] = row.English
+        data_as_dict = data.to_dict(orient="records")
 
-            # OR (instead of the FOR loop
-        # data_as_dict = data.to_dict(orient="records")
+        # OR (using a FOR loop)
+
+        # for (index, row) in data.iterrows():
+        #     data_as_dict[row.French] = row.English
 
 
 # ------------------- generate next word -------------#
-def next_word():
-    # put all the keys in a list
-    try:
-        keys_list = list(data_as_dict.keys())
-    except ValueError:
-        # no more entries available
+def next_card():
+    # generate a number and get the key
+    if data_as_dict:
+        return random.choice(data_as_dict)
+    else:  # no more entries available
+        win.after_cancel(flip_timer)
         messagebox.showinfo(title="Congratulations!", message="Good job! French flash cards remembered!")
-    else:
+        # remove temp file
         try:
-            # generate a number and get the key
-            next_word_x = keys_list[random.randint(0, len(keys_list) - 1)]
-        except ValueError:
-            return None
-        else:
-            return next_word_x
+            os.remove("data/words_to_learn.csv")
+        except FileNotFoundError:
+            pass
 
 
 # -------------- display word on flash card ----------#
@@ -103,15 +108,13 @@ front_image = PhotoImage(file="images/card_front.png")
 
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 canvas_image = canvas.create_image(400, 263, image=front_image)
-language_id = canvas.create_text(400, 150, text="French", font=("Ariel", 48, "italic"))
-current_fr_word = next_word()
-word_id = canvas.create_text(400, 263, text=current_fr_word, font=("Ariel", 60, "bold"))
+language_id = canvas.create_text(400, 150, text="French", font=("Ariel", 38, "italic"))
+
+word_id = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 flip_timer = win.after(0, flip_to_front_side)
 
 canvas.config(bg=BACKGROUND_COLOR)
 canvas.grid(column=0, row=0, columnspan=2)
-
-# TODO - find a way to disable a canvas from being displayed
 
 right_image = PhotoImage(file="images/right.png")
 right_button = Button(image=right_image, highlightthickness=0, command=remembered)
@@ -120,5 +123,7 @@ right_button.grid(column=1, row=1)
 false_image = PhotoImage(file="images/wrong.png")
 false_button = Button(image=false_image, highlightthickness=0, command=failed)
 false_button.grid(column=0, row=1)
+
+flip_to_front_side()
 
 win.mainloop()
